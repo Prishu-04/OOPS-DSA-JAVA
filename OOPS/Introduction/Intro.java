d9582b4d818fc2e4b585627630260ed6efad33c2
Prishu-04 <pratyakshsrivastava@23ksgmail.com>
2026-01-23 21:03:54 +0530

    Lab update

---
+++ OOPS/Introduction/Intro.java
@@ -0,0 +1,20 @@
+public Into{
+     public static void main(String[] args){
+          //store 5 roll no.
+          int[] num= new int[5];
+          // Store 5 names
+          String[] name=new String[5];
+          // data of 5 students : (roll no, name, marks)
+          int [] rno = new int[5];
+          String [] name= new String[5];
+          float[] marks=new float[5];
+
+          Student[] student =new Students[5];
+          Student Prishu;
+     }
+     class Student{
+          int [] rno = new int[5];
+          String [] name= new String[5];
+          float[] marks=new float[5];
+     }
+}
